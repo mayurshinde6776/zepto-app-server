b55edc74b176af4cdbb8a1df415cd033d1dfe014
Mayur-Thinkbridge <mayurshinde6776@gmail.com>
2024-03-12 16:56:11 +0530

    success and cancel page urls changed

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
         payment_method_types: ["card"],
         line_items: lineItems,
         mode: "payment",
-        success_url: "http://localhost:3000/success",
-        cancel_url: "http://localhost:3000/cancel"
+        success_url: "https://zepto-app-client.onrender.com/success",
+        cancel_url: "https://zepto-app-client.onrender.com/cancel"
     });
 
     res.json({ id: session.id });
